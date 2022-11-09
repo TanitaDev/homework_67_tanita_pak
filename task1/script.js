b63a1b8636435f3b7c8b3ac70ce1a9ab60165fdd
Tanita <tanita.pak@gmail.com>
2022-11-10 01:58:52 +0600

    написала открытие и закрытие окна уведомления

--- task1/script.js
+++ task1/script.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
     $("#modal-window").hide()
     $("#add-item-btn").on('click', function () {
         $("#modal-window").show()
+        setTimeout(function () { })
     })
 
     $("#close-btn").on('click', function () {
         $("#modal-window").hide()
     })
+    setTimeout(function () { $("#modal-window").hide(); }, 2000);
 })
