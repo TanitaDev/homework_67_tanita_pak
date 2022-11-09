f8cd3ee2c4bc0777cecef7a6d3b465eff60ee937
tanitaDev <tanita.pak@gmail.com>
2022-11-10 02:22:31 +0600

    добавила таймер для закрытия окна уведомления

--- task1/script.js
+++ task1/script.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     $("#modal-window").hide()
     $("#add-item-btn").on('click', function () {
         $("#modal-window").show()
-        setTimeout(function () { })
+        setTimeout(function () { $("#modal-window").hide(); }, 5000);
     })
 
     $("#close-btn").on('click', function () {
         $("#modal-window").hide()
     })
-    setTimeout(function () { $("#modal-window").hide(); }, 2000);
+
 })
