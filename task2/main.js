fff49f6d2e88a8aa4660e6bcb2b2257bc9e6eb6e
tanitaDev <tanita.pak@gmail.com>
2022-11-11 18:43:40 +0600

    написала функцию для отображения текущего возраста

--- task2/main.js
+++ task2/main.js
@@ -15,27 +15,43 @@ fetch(url = 'https://www.breakingbadapi.com/api/characters')
         }
     })
 
-let urlParams = new URLSearchParams(window.location.search);
-let param = urlParams.get('id');
 
-fetch(url = `https://www.breakingbadapi.com/api/characters/${param}`)
+function currenAge(birthdate) {
+    var now = new Date();
+    var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    var dob = new Date(birthdate);
+    var dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate());
+    var age;
+
+    age = today.getFullYear() - dob.getFullYear();
+    if (today < dobnow) {
+        age = age - 1;
+    }
+    return age
+}
+
+let urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get('id');
+
+fetch(url = `https://www.breakingbadapi.com/api/characters/${id}`)
     .then(response => {
         return response.json();
     })
     .then(data => {
         for (let i = 0; i < data.length; i++) {
             let char_data = data[i]
+
             let markUp = `<div class="img">
                         <img src="${char_data['img']}" alt="" />
                         </div>
                         <div class="info_block"> 
                             <p class="name">Name: <b>${char_data['name']}</b>;
                             <span class="nickname"> Nickname: <b>${char_data['nickname']}</b></span></p>
-                            <p class="age">Age <b>${char_data['birthday']}</b></p>
-                            <p class="occupation">Occupation <b>${char_data['occupation']}</b></p>
-                            <p class="status">Status <b>${char_data['status']}</b></p>
-                            <p class="portrayed">Portrayed by <b>${char_data['portrayed']}</b></p>
-                            <p class="category">Category <b>${char_data['category']}</b></p>
+                            <p class="age">Age: <b>${currenAge(char_data['birthday'])} years</b></p>
+                            <p class="occupation">Occupation: <b>${char_data['occupation']}</b></p>
+                            <p class="status">Status: <b>${char_data['status']}</b></p>
+                            <p class="portrayed">Portrayed by: <b>${char_data['portrayed']}</b></p>
+                            <p class="category">Category: <b>${char_data['category']}</b></p>
                         </div>`
 
             $(".character_block").append(markUp)
